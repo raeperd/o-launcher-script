e3152a9dcf99712fd49cc8749e53fcf1f9c71658
raeperd <raeperd117@gmail.com>
2025-03-18 16:46:47 +0900

    feat: Fix Zen link to Vivaldi

--- CreateLauncherModeTemplate.rb
+++ CreateLauncherModeTemplate.rb
@@ -19,7 +19,7 @@ end
 require 'json'
 
 def main
-  version = 'v1.8.2'
+  version = 'v1.8.3'
   title = 'o-launcher ' << version
   rule = {
     'description' => 'o-launcher ' << version,
@@ -36,7 +36,7 @@ def main
       generate_launcher_mode('s', [], [{ 'shell_command' => "open -a Spotify.app" }]),
       generate_launcher_mode('t', [], [{ 'shell_command' => "open -a TickTick.app" }]),
       generate_launcher_mode('v', [], [{ 'shell_command' => "open -a 'Visual Studio Code.app'" }]),
-      generate_launcher_mode('z', [], [{ 'shell_command' => "open -a 'Zen.app'" }]),
+      generate_launcher_mode('z', [], [{ 'shell_command' => "open -a 'Vivaldi.app'" }]),
     ].flatten,
   }
 
